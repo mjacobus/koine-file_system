06fbd6b00ef528f380c0488fa05575fa24ed712b
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2020-04-21 16:05:54 -0300

    Please rubocop

--- spec/koine/file_system/path_sanitizer_spec.rb
+++ spec/koine/file_system/path_sanitizer_spec.rb
@@ -6,11 +6,9 @@ RSpec.describe Koine::FileSystem::PathSanitizer do
   subject(:sanitizer) { described_class.new }
 
   describe '#sanitize' do
-    SAMPLES = {
+    {
       '.././foo/bar/.././../../file.txt' => 'foo/bar/file.txt'
-    }.freeze
-
-    SAMPLES.each do |key, value|
+    }.each do |key, value|
       it "transforms #{key} in #{value}" do
         expect(sanitizer.sanitize(key)).to eq(value)
       end
